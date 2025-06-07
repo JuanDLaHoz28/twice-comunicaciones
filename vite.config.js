72ee25c86e814805dfa9d4fd6ad4a8d59cbf2476
Juan Pablo De La Hoz Ruiz <juan.delahoz16@uac.edu.co>
2025-06-07 16:55:36 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/twice-comunicaciones/', // <-- ¡ESTO ES CLAVE!
+  base: '/twice-comunicaciones/',
   plugins: [react()],
   server: {
     port: 3000,
